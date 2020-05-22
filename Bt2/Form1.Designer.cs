83afe22c5315640dc1d70ef00c7d4bcbabb6dc55
FGA_Training <HERA.fgatraining@fsoft.com.vn>
2020-05-22 14:19:51 +0700

    Update Form1.Designer.cs

--- Bt2/Form1.Designer.cs
+++ Bt2/Form1.Designer.cs
@@ -28,13 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(41, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Listen...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(55, 110);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(634, 279);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
     }
 }
 
