e14567006622ce79cc5e7263cc3250b893d3b1a3
FGA_Training <HERA.fgatraining@fsoft.com.vn>
2020-05-22 14:19:31 +0700

    Update Form2.cs

--- Bt1/Form2.cs
+++ Bt1/Form2.cs
@@ -26,5 +26,10 @@ namespace Bt1
             Byte[] sendBytes = Encoding.ASCII.GetBytes(richTextBox1.Text);
             udpClient.Send(sendBytes, sendBytes.Length);
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
