f78b5ba6953de6c7ff64ca6ff5751b4c5a75bfc1
FGA_Training <HERA.fgatraining@fsoft.com.vn>
2020-05-22 14:19:35 +0700

    Update Form1.cs

--- Bt2/Form1.cs
+++ Bt2/Form1.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
 
 namespace Bt2
 {
@@ -15,6 +19,58 @@ namespace Bt2
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //Xử lý lỗi InvalidOperationException
+            CheckForIllegalCrossThreadCalls = false;
+            Thread serverThread = new Thread(new ThreadStart(StartUnsafeThread));
+            serverThread.Start();
+        }
+        void StartUnsafeThread()
+        {
+            int bytesReceived = 0;
+            // Khởi tạo mảng byte nhận dữ liệu
+            byte[] recv = new byte[1];
+            // Tạo socket bên gởi
+            Socket clientSocket;
+            // Tạo socket bên nhận, socket này là socket lắng nghe các kết nối tới nó tại địa chỉ IP của
+            //máy và port 8080.Đây là 1 TCP / IP socket.
+            //AddressFamily: trả về họ địa chỉ của địa chỉ IP hiện hành. Ở đây là địa chỉ Ipv4 nên chọn:
+
+            //1- AddressFamily.InterNetwork
+            //2- SocketType: kiểu kết nối socket, ở đây dùng luồng Stream để nhận dữ liệu
+            //3- ProtocolType: sử dụng giao thức kết nối nào, ở đây sử dụng kết nối TCP
+
+            // Ba tham số của hàm đi với nhau khi ta thực hiện kết nối TCP.
+            Socket listenerSocket = new Socket(
+                AddressFamily.InterNetwork,
+                SocketType.Stream,
+                ProtocolType.Tcp
+                );
+            IPEndPoint ipepServer = new IPEndPoint(IPAddress.Parse("192.168.1.2"), 8080);
+            // Gán socket lắng nghe tới địa chỉ IP của máy và port 8080
+            listenerSocket.Bind(ipepServer);
+            // bắt đầu lắng nghe. Socket.Listen(int backlog)
+            // với backlog: là độ dài tối đa của hàng đợi các kết nối đang chờ xử lý
+            listenerSocket.Listen(-1);
+            //Đồng ý kết nối
+            clientSocket = listenerSocket.Accept();
+            //Nhận dữ liệu
+            listView1.Items.Add(new ListViewItem("New client connected"));
+            while (clientSocket.Connected)
+            {
+                string text = "";
+                do
+                {
+                    bytesReceived = clientSocket.Receive(recv);
+                    text += Encoding.ASCII.GetString(recv);
+                } while (text[text.Length - 1] != '\n');
+                listView1.Items.Add(new ListViewItem(text));
+
+            }
+            listenerSocket.Close();
         }
     }
 }
