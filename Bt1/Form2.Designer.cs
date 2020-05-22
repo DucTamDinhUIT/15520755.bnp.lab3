d56534ff36bd6aa4a9a7dfcb44140cb63aa0db51
FGA_Training <HERA.fgatraining@fsoft.com.vn>
2020-05-22 14:19:28 +0700

    Update Form2.Designer.cs

--- Bt1/Form2.Designer.cs
+++ Bt1/Form2.Designer.cs
@@ -46,12 +46,13 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(455, 66);
+            this.button1.Location = new System.Drawing.Point(412, 48);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(118, 58);
             this.button1.TabIndex = 1;
             this.button1.Text = "Listen";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // richTextBox1
             // 
@@ -65,13 +66,13 @@
             // 
             this.textBox1.Location = new System.Drawing.Point(112, 68);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(269, 20);
+            this.textBox1.Size = new System.Drawing.Size(230, 20);
             this.textBox1.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(37, 111);
+            this.label2.Location = new System.Drawing.Point(109, 112);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(111, 13);
             this.label2.TabIndex = 4;
